f105d27a8b03a56976e471d26626566f4ea42d36
MajesticPotatoe <amajesticpotatoe@gmail.com>
2019-09-19 19:55:24 -0400

    style: fix linting

--- src/components/app/AppSystemBar.vue
+++ src/components/app/AppSystemBar.vue
@@ -1,9 +1,15 @@
 <template>
   <v-system-bar app window lights-out>
     <v-spacer />
-    <v-icon class="systemBarBtn">mdi-window-minimize</v-icon>
-    <v-icon class="systemBarBtn">mdi-window-maximize</v-icon>
-    <v-icon class="systemBarBtn">mdi-window-close</v-icon>
+    <v-icon class="systemBarBtn">
+      mdi-window-minimize
+    </v-icon>
+    <v-icon class="systemBarBtn">
+      mdi-window-maximize
+    </v-icon>
+    <v-icon class="systemBarBtn">
+      mdi-window-close
+    </v-icon>
   </v-system-bar>
 </template>
 
