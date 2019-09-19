d6005a528d5b68745d7382be03788342e71a55a3
MajesticPotatoe <amajesticpotatoe@gmail.com>
2019-09-19 19:54:56 -0400

    feat: create basic system bar

--- src/background.js
+++ src/background.js
@@ -3,7 +3,7 @@
 import { app, protocol, BrowserWindow } from 'electron'
 import {
   createProtocol,
-  installVueDevtools
+  installVueDevtools,
 } from 'vue-cli-plugin-electron-builder/lib'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -12,13 +12,18 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 let win
 
 // Scheme must be registered before the app is ready
-protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true, standard: true } }])
+protocol.registerSchemesAsPrivileged([{ scheme: 'app', privileges: { secure: true, standard: true } }])
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600, webPreferences: {
-    nodeIntegration: true
-  } })
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  })
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
---
+++ src/components/app/AppSystemBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-system-bar app window lights-out>
+    <v-spacer />
+    <v-icon class="systemBarBtn">mdi-window-minimize</v-icon>
+    <v-icon class="systemBarBtn">mdi-window-maximize</v-icon>
+    <v-icon class="systemBarBtn">mdi-window-close</v-icon>
+  </v-system-bar>
+</template>
+
+<script>
+  export default {
+    name: 'AppSystemBar',
+    data: () => ({
+      //
+    }),
+  }
+</script>
+
+<style lang="sass">
+  .systemBarBtn
+    width: 30px
+    padding: 0 !important
+    margin: 0 !important
+</style>
