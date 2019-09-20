d1b4667ce7b662caff48aea205d8cc510f1e7d7b
MajesticPotatoe <amajesticpotatoe@gmail.com>
2019-09-19 23:46:28 -0400

    fix: fix naming typo on landing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Vuetify-Electron
+          Electron-Vuetify
         </h1>
         <p class="subheading font-weight-regular">
           A lightweight boilerplate for electron 6 and vuetify 2.x
