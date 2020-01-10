3a2330e3db27aa99210f858cf4f97b20b6b4104c
MajesticPotatoe <amajesticpotatoe@gmail.com>
2020-01-09 19:27:42 -0500

    fix: electron should run without error

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      chainWebpackRendererProcess: config => {
+        if (process.env.NODE_ENV === 'development') {
+          config.plugins.delete('prefetch')
+        }
+      },
+    },
+  },
+  transpileDependencies: [
+    'vuetify',
+  ],
+}
