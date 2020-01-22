7cbebe0da46290ede359f95330d4321d17f641f3
Andrew Henry <ahenry@fcp.biz>
2020-01-22 16:17:12 -0500

    chore: add example build parameters

--- vue.config.js
+++ vue.config.js
@@ -6,9 +6,20 @@ module.exports = {
           config.plugins.delete('prefetch')
         }
       },
+      // example build parameters
+      mainProcessFile: 'src/background.js',
+      builderOptions: {
+        productName: 'ElectronVuetify',
+        appId: 'org.majesticportatoe.electronvuetify',
+        win: {
+          icon: 'build/icons/icon.ico',
+          target: 'portable',
+        },
+        files: [],
+      },
     },
+    transpileDependencies: [
+      'vuetify',
+    ],
   },
-  transpileDependencies: [
-    'vuetify',
-  ],
 }
