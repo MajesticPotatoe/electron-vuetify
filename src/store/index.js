ed3a11f9766c41a7605a6b6bb2e89da639202dbb
Andrew Henry <ahenry@fcp.biz>
2020-01-22 16:08:36 -0500

    fix: store modules should load

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,14 @@
-let window = {}
-if (process.env.IS_ELECTRON) {
-  window = (() => (require('./window').default))()
-}
-export { window }
+// Imports
+import Vue from 'vue'
+import Vuex from 'vuex'
+import pathify from 'vuex-pathify'
+
+import * as modules from './modules'
+
+pathify.options.mapping = 'simple'
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  plugins: [pathify.plugin],
+  modules,
+})
